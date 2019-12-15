dae9f95cb34ee6aeb710a04e6581a1283885daa0
Matheus Kindrazki <matheuskindrazki@gmail.com>
2019-12-15 03:31:54 +0000

    criando rota para admin

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,13 @@ import { Router } from 'express';
 
 const routes = new Router();
 
+
+routes.get(['/consultora', '/consultora/*', '/consultora.htm'], async (req, res) => {
+  return res.sendFile(`${process.env.ADMIN_PATH}/index.html`);
+});
+
+routes.get(['/admin', '/admin/*', '/admin.html'], async (req, res) => {
+  return res.sendFile(`${process.env.CONSULTORA_PATH}/index.html`);
+});
+
 export default routes;
